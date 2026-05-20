58141537f871bd6dd890df95d77b03e0ac5242fb
Hammad Ali <thathammadali@gmail.com>
2026-05-21 04:00:03 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn How to Use GitHub!
         </a>
       </header>
     </div>
